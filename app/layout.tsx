42fddb4be2a52a10b2a7129174acdb4ed2580668
madhesh04 <madheshprasath@gmail.com>
2026-02-11 23:29:27 +0530

    feat: Initialize Antique Gem website with core pages, UI components, styling, and assets.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Outfit, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -42,6 +43,7 @@ export default function RootLayout({
         className={`${inter.variable} ${outfit.variable} ${montserrat.variable} antialiased bg-light-grey text-maroon`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
